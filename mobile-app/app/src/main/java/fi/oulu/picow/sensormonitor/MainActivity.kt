3914149dda1fbd4c7080d55fea700204259f33bb
Teemu Lahtinen <teemu.lahtinen@ioliving.com>
2025-11-26 13:35:08 +0200

    Add Android mobile app to project

---
+++ mobile-app/app/src/main/java/fi/oulu/picow/sensormonitor/MainActivity.kt
@@ -0,0 +1,47 @@
+package fi.oulu.picow.sensormonitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fi.oulu.picow.sensormonitor.ui.theme.PicoWSensorMonitorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PicoWSensorMonitorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PicoWSensorMonitorTheme {
+        Greeting("Android")
+    }
+}
